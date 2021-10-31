61caeefbd4bafb6f930608324ff86614ef34ede9
Johnny SH Wu <Johnny_SH_Wu@wistron.com>
2021-10-31 19:11:04 +0800

    update

--- demo/src/test/java/com/example/demo/EasyMethodTest.java
+++ demo/src/test/java/com/example/demo/EasyMethodTest.java
@@ -26,4 +26,12 @@ public class EasyMethodTest {
         Assert.assertEquals(expected, result);
     }
 
+    @Test
+    public void test() {
+        System.out.println(0.06 + 0.01);
+        System.out.println(1.0 - 0.42);
+        System.out.println(4.015 * 100);
+        System.out.println(123.3 / 100);
+    }
+
 }
