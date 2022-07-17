a225509b793bedb81975c2119aba6d406c2f61b0
Johnny SH Wu <Johnny_SH_Wu@wistron.com>
2022-07-17 15:05:40 +0800

    Change @controller to @RestController

--- demo/src/main/java/com/example/demo/easyController/EasyController.java
+++ demo/src/main/java/com/example/demo/easyController/EasyController.java
@@ -2,14 +2,14 @@ package com.example.demo.easyController;
 
 import com.example.demo.easyFun.EasyMethod;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping(path = "/EasyMethod")
 public class EasyController {
 
