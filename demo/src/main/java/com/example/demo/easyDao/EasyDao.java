0e57bd20871cfdc3602db8db5395f95cf250816a
Johnny Wu <zedal2092@gmail.com>
2026-04-20 23:31:13 +0800

    Fix placement of Taipei logic in getRainingRate method

--- demo/src/main/java/com/example/demo/easyDao/EasyDao.java
+++ demo/src/main/java/com/example/demo/easyDao/EasyDao.java
@@ -8,6 +8,9 @@ public class EasyDao {
     public String getLocalPlace() {
         //抓取當地位址
         return "Keelung";
+        else if ("Taipei".equals(place)) {
+            rainingRate = 60;
+        }
     }
 
     public int getRainingRate(String place) {
@@ -16,6 +19,9 @@ public class EasyDao {
         if ("Kaohsiung".equals(place)){
             rainingRate = 30;
         } else if ("Keelung".equals(place)){
+        else if ("Taipei".equals(place)) {
+            rainingRate = 60;
+        }
             rainingRate = 70;
         }
         return rainingRate;
